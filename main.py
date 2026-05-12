f8f9781d0df9b0e56c68a45af90c8c217b4361f3
maaz <maazk2414@gmail.com>
2026-05-12 23:25:06 +0530

    complete curd methods

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from fastapi import FastAPI
-
-
+from pydantic import BaseModel
 app = FastAPI()
 
-#home route
-@app.get("/")
-def home():
-    return {"welcome to faast api "}
 
-#about route
-@app.get("/about")
-def about():
-    return{"message:this my about route"}
+Todos = []
 
-@app.get("/user/{user_id}/{rollno}")
-def get_user(user_id,rollno):
-    return {"user_id": user_id,"rollno":rollno}
+class Todo(BaseModel):
+    id : int
+    title : str
+    completed : bool    
+
+@app.post("/todos")
+def create_todo(todo:Todo):
+    Todos.append(todo)
+    return {"message" : "TODO added","Data ":todo}
+
+
+@app.get("/todos")
+def get_info():
+    return{
+        "meassage " : "list of data",
+        "data" : Todos
+    }
