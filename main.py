eed44c0b4de0f8baa0996b09cbc1b25b997818d7
maaz <maazk2414@gmail.com>
2026-05-15 13:07:57 +0530

    connect database

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI,Depends
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, declarative_base,Session
+
+
+
 app = FastAPI()
 
+DATABASE_URL =  "sqlite:///./test.db"
 
-Todos = []
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread":False}
+)
 
-class Todo(BaseModel):
-    id : int
-    title : str
-    completed : bool    
-
-@app.post("/todos")
-def create_todo(todo:Todo):
-    Todos.append(todo)
-    return {"message" : "TODO added","Data ":todo}
+sessionLocal = sessionmaker(bind = engine)
+Base = declarative_base()    
 
 
-@app.get("/todos")
-def get_info():
+class Todo(Base):
+    __tablename__ = "todos"
+    id = Column(Integer,primary_key = True, index = True)
+    title = Column(String)
+    completed = Column(String)
+
+
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/")
+def home(db: Session = Depends(get_db)):
     return{
-        "meassage " : "list of data",
-        "data" : Todos
+        "message":"DB connected fine"
     }
